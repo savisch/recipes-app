e95f5a73f50b9faece4740376c092356920d56a3
savisch <swsolns@gmail.com>
2023-12-27 11:07:47 -0600

    Adds recipe edit component

---
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../models/recipe.model';
+import { RecipeService } from '../services/recipes.service';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrl: './recipe-edit.component.css',
+})
+export class RecipeEditComponent {
+  recipeId: number;
+  recipe: Recipe;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    this.recipeId = +this.route.snapshot.params['id'];
+    this.getRecipe();
+  }
+
+  getRecipe(): void {
+    this.recipe = this.recipeService.getRecipe(this.recipeId);
+  }
+}

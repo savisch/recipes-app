2dbf156c130bf5cb5fd8fc19cb7041b4a87d598e
savisch <swsolns@gmail.com>
2023-12-27 16:39:41 -0600

    Add content to recipe book home

--- src/app/recipe-book-home/recipe-book-home.component.ts
+++ src/app/recipe-book-home/recipe-book-home.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-book-home',
   templateUrl: './recipe-book-home.component.html',
-  styleUrl: './recipe-book-home.component.css'
+  styleUrl: './recipe-book-home.component.css',
 })
-export class RecipeBookHomeComponent {
-
-}
+export class RecipeBookHomeComponent {}

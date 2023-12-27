c2234f47350b516dc7cf73c0eb59f7e83ece83b1
savisch <swsolns@gmail.com>
2023-12-27 16:40:54 -0600

    Add child paths, auth guards, and resolvers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { childAuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RecipeBookHomeComponent } from './recipe-book-home/recipe-book-home.component';
+import { recipeResolver } from './recipe.resolver';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { RecipeComponent } from './recipe/recipe.component';
@@ -13,20 +17,33 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipeComponent,
+    canActivateChild: [childAuthGuard],
     children: [
-      { path: 'recipe-detail/:id', component: RecipeDetailComponent },
-      { path: 'recipe-edit/:id', component: RecipeEditComponent },
+      {
+        path: 'recipe-detail/:id',
+        component: RecipeDetailComponent,
+        resolve: { recipe: recipeResolver },
+      },
+      {
+        path: 'recipe-edit/:id',
+        // canActivate: [authGuard],
+        component: RecipeEditComponent,
+        resolve: { recipe: recipeResolver },
+      },
     ],
   },
   { path: 'shopping', component: ShoppingListComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: 'not-authorized', component: NotAuthorizedComponent },
   { path: '', redirectTo: '/recipe-book', pathMatch: 'full' },
   { path: '**', redirectTo: '/page-not-found' },
 ];
 
 @NgModule({
   declarations: [],
+  // imports: [CommonModule, RouterModule.forRoot(appRoutes, { useHash: true })],
   imports: [CommonModule, RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
+  providers: [AuthService],
 })
 export class AppRoutingModule {}

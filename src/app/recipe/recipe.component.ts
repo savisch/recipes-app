aefb396852639118c2c107a935ed17bbdcdbaae4
savisch <swsolns@gmail.com>
2023-12-27 11:06:43 -0600

    Adds and populates recipe component

---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrl: './recipe.component.css',
+})
+export class RecipeComponent {}

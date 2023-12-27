fc82ebc3c0c8c73003327a9b32ad488b53e41481
savisch <swsolns@gmail.com>
2023-12-27 11:07:04 -0600

    Adds and populates header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  // @Output() buttonClick = new EventEmitter<string>();
+  // onClick(buttonLabel: string) {
+  //   this.buttonClick.emit(buttonLabel);
+  // }
+}

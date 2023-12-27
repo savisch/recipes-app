b5368e79d1e5725aa7558aaac636065c1988a005
savisch <swsolns@gmail.com>
2023-12-27 11:09:56 -0600

    Adds recipe book home component

---
+++ src/app/recipe-book-home/recipe-book-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-book-home',
+  templateUrl: './recipe-book-home.component.html',
+  styleUrl: './recipe-book-home.component.css'
+})
+export class RecipeBookHomeComponent {
+
+}

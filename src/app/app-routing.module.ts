ba365f363a5825cabd8abf55157cc4da54e6f7ef
savisch <swsolns@gmail.com>
2023-12-28 09:02:12 -0600

    Add title property to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { childAuthGuard } from './auth.guard';
+import { authGuard, childAuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -13,28 +13,47 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-  { path: 'recipe-book', component: RecipeBookHomeComponent },
+  {
+    path: 'recipe-book',
+    title: 'Recipe Book',
+    component: RecipeBookHomeComponent,
+  },
   {
     path: 'recipes',
+    title: 'Recipe List',
     component: RecipeComponent,
     canActivateChild: [childAuthGuard],
     children: [
       {
         path: 'recipe-detail/:id',
+        title: 'Recipe Detail',
         component: RecipeDetailComponent,
         resolve: { recipe: recipeResolver },
       },
       {
         path: 'recipe-edit/:id',
-        // canActivate: [authGuard],
+        title: 'Recipe Edit',
         component: RecipeEditComponent,
         resolve: { recipe: recipeResolver },
       },
     ],
   },
-  { path: 'shopping', component: ShoppingListComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: 'not-authorized', component: NotAuthorizedComponent },
+  {
+    path: 'shopping',
+    canActivate: [authGuard],
+    title: 'Shopping List',
+    component: ShoppingListComponent,
+  },
+  {
+    path: 'page-not-found',
+    title: 'Not Found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: 'not-authorized',
+    title: 'Not Authorized',
+    component: NotAuthorizedComponent,
+  },
   { path: '', redirectTo: '/recipe-book', pathMatch: 'full' },
   { path: '**', redirectTo: '/page-not-found' },
 ];
